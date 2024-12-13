cf156eec854c15937bf72ef139f7460ed36853e4
Nessa <144719207+Nessa0716@users.noreply.github.com>
2024-12-13 15:59:41 +0000

    #3) Create a variable for a title, owner, and a GitHub repository link.

--- site.js
+++ site.js
@@ -35,9 +35,9 @@ const vue_app = Vue.createApp({
             
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            title: "IMDB + Vanessa’s Top 8 Movies",
+            title: "IMDB + Vanessa's Top 8 Movies",
             owner: "Vanessa Ogno",
-            github: "https://github.com/Nessa0716",
+            github: "https://github.com/Nessa0716/NJIT-2",
       }
     },
       methods: {
