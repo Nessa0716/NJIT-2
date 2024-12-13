b1acdfc9e6e318b3ca90f29a89e8855d95e1b54f
Nessa <144719207+Nessa0716@users.noreply.github.com>
2024-12-13 16:41:13 +0000

    #5) Fix the syntax for the data-bindings for the remaining variables used for the movies and their information using Vue syntax.

--- site.js
+++ site.js
@@ -30,19 +30,65 @@ const vue_app = Vue.createApp({
             })
       },
       data() {
-        return {
-            // This holds your movies.json data.
-            
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            title: "IMDB + Vanessa's Top 8 Movies",
-            owner: "Vanessa Ogno",
-            github: "https://github.com/Nessa0716/NJIT-2",
-      }
+            return {
+                  // This holds your movies.json data.
+                  
+                  movies: [],
+                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+                  title: "IMDB + Vanessa's Top 8 Movies",
+                  owner: "Vanessa Ogno",
+                  github: "https://github.com/Nessa0716/NJIT-2",
+            }
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray) {
+
+                  let year = dateArray[0];
+                  let day = dateArray[2];
+                  switch (dateArray[1]) {
+                        case 1:
+                              month = "January";
+                              break;
+                        case 2:
+                              month = "February";
+                              break;
+                        case 3:
+                              month = "March";
+                              break;
+                        case 4:
+                              month = "April";
+                              break;
+                        case 5:
+                              month = "May";
+                              break;
+                        case 6:
+                              month = "June";
+                              break;
+                        case 7:
+                              month = "July";
+                              break;
+                        case 8:
+                              month = "August";
+                              break;
+                        case 9:
+                              month = "September";
+                              break;
+                        case 10:
+                              month = "October";
+                              break;
+                        case 11:
+                              month = "November";
+                              break;
+                        case 12:
+                              month = "December";
+                              break;
+                  }
+                  return `${month} ${day}, ${year}`
+            }
       }
 })
 
 vue_app.mount("#vue_app")
+
+
